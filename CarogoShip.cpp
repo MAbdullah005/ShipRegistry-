1b0a802dc7c3429c56e95dc3e63c6d3d54c28b19
Abdullah Ali <162645001+MAbdullah005@users.noreply.github.com>
2024-10-27 23:03:58 -0700

    Update CarogoShip.cpp

--- CarogoShip.cpp
+++ CarogoShip.cpp
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+#ifndef CARGOSHIP_H
+#define CARGOSHIP_H
 
-namespace ConsoleApp1
-{
-    internal class CarogoShip
-    {
+#include "Ship.h"
+
+class CargoShip : public Ship {
+private:
+    int cargoCapacity;
+public:
+    CargoShip(std::string n, std::string y, int c) : Ship(n, y), cargoCapacity(c) {}
+    void setCargoCapacity(int c) { cargoCapacity = c; }
+    int getCargoCapacity() const { return cargoCapacity; }
+    void print() const override {
+        std::cout << "Cargo Ship Name: " << getName() << ", Cargo Capacity: " << cargoCapacity << " tons" << std::endl;
     }
-}
+};
+
+#endif
