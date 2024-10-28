509f226e6cdbf3201510d37e6ebd9b34d1de4c60
Abdullah Ali <162645001+MAbdullah005@users.noreply.github.com>
2024-10-27 23:02:52 -0700

    Update ship.cpp

--- ship.cpp
+++ ship.cpp
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+#ifndef SHIP_H
+#define SHIP_H
+#include <iostream>
+#include <string>
 
-namespace ConsoleApp1
-{
-    internal class ship
-    {
+class Ship {
+private:
+    std::string name;
+    std::string yearBuilt;
+public:
+    Ship(std::string n, std::string y) : name(n), yearBuilt(y) {}
+    void setName(std::string n) { name = n; }
+    std::string getName() const { return name; }
+    void setYearBuilt(std::string y) { yearBuilt = y; }
+    std::string getYearBuilt() const { return yearBuilt; }
+    virtual void print() const {
+        std::cout << "Ship Name: " << name << ", Year Built: " << yearBuilt << std::endl;
     }
-}
+};
+
+#endif
